49fcb031f1a55fce50fda536da9c0029a5568966
Erfan_shawon <devwitherfan99@gmail.com>
2026-01-17 20:21:59 +0600

    updated!

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   const progressBars = document.querySelectorAll(".progress-container");
 
@@ -53,6 +54,7 @@ const visible = getVisibleCards();
 if (index > 0) index--;
 track.style.transform = `translateX(-${index * (100 / visible)}%)`;
 }
+
 // *my work slider ends
 
 var typed = new Typed('.typed', 
@@ -64,6 +66,60 @@ var typed = new Typed('.typed',
     startDelay: 200,
     loop: true,
   });
+ 
+  $(document).ready(function () {
+    let index = 0;
+    const slides = $('.feedback-item');
+    const dots = $('.dot');
+
+    slides.hide().eq(0).show(); // first slide visible
+
+    setInterval(function () {
+      let current = slides.eq(index);
+
+      // fade out + move right
+      current.animate(
+        {
+          opacity: 0,
+          marginRight: '40px'
+        },
+        600,
+        function () {
+          current.hide().css({
+            opacity: 1,
+            marginRight: '0'
+          });
+
+          dots.eq(index).removeClass('active');
+
+          index = (index + 1) % slides.length;
+
+          let next = slides.eq(index);
+
+          // start from left
+          next.css({
+            display: 'block',
+            opacity: 0,
+            marginRight: '-60px'
+          });
+
+          // fade in + move to center
+          next.animate(
+            {
+              opacity: 1,
+              marginRight: '0'
+            },
+            600
+          );
+
+          dots.eq(index).addClass('active');
+        }
+      );
+    }, 2000);
+  });
+
 
    AOS.init();
+
+   
 
